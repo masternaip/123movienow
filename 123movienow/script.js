8e1cb030184b666438cd9b53751d74e05e3a7120
masternaip <167619502+masternaip@users.noreply.github.com>
2025-05-25 04:09:21 +0800

    Update script.js

--- 123movienow/script.js
+++ 123movienow/script.js
@@ -1,5 +1,5 @@
 // ==== Configuration ====
-const API_KEY = 'YOUR_TMDB_API_KEY'; // <-- Replace with your TMDb API key
+const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae'; // Your TMDb API Key
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 

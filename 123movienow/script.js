5713faefa78bb5f64208f0ce6eb480a1b87d0ac2
masternaip <167619502+masternaip@users.noreply.github.com>
2025-05-25 05:29:31 +0800

    Update script.js

--- 123movienow/script.js
+++ 123movienow/script.js
@@ -1,6 +1,7 @@
 const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae'; // Your TMDb API Key
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+let currentItem; // To store the currently selected item for modal details
 
 // === FETCH FUNCTIONS ===
 
